2a05226f54f1073723616f8edc2662cd4387ba35
Flayerbut <rapriadi706@gmail.com>
2025-06-15 22:44:13 +0700

    Add files via upload

---
+++ lapangan/LapanganIndoor.java
@@ -0,0 +1,7 @@
+package lapangan;
+
+public class LapanganIndoor extends Lapangan {
+    public LapanganIndoor(String id, String nama, String lokasi, double hargaPerJam, String[] fasilitas) {
+        super(id, nama, lokasi, hargaPerJam);
+    }
+}
---
+++ lapangan/LapanganOutdoor.java
@@ -0,0 +1,7 @@
+package lapangan;
+
+public class LapanganOutdoor extends Lapangan {
+    public LapanganOutdoor(String id, String nama, String lokasi, double hargaPerJam) {
+        super(id, nama, lokasi, hargaPerJam);
+    }
+}
